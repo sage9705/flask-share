e8b9fbb7ad9b6e1c6acf09f6d3ddad1d06b7a100
Sage9705 <edem22kumahor@gmail.com>
2024-03-30 19:38:16 +0000

    multiple files upload enabled

--- app.py
+++ app.py
@@ -15,21 +15,26 @@ def index():
     port = 5000  # You can choose any available port
     return render_template('index.html', ip_address=ip_address, port=port)
 
+
 @app.route('/upload', methods=['POST'])
 def upload_file():
     try:
         if 'file' not in request.files:
             return "No file part", 400
 
-        file = request.files['file']
-        if file.filename == '':
-            return "No selected file", 400
+        files = request.files.getlist('file')  # Get a list of uploaded files
+        for file in files:
+            if file.filename == '':
+                return "No selected file", 400
 
-        filename = secure_filename(file.filename)
-        file.save(os.path.join('uploads', filename))
-        return "File uploaded successfully!"
+            filename = secure_filename(file.filename)
+            file.save(os.path.join('uploads', filename))
+
+        return "Files uploaded successfully!"
     except Exception as e:
-        return f"Error uploading file: {str(e)}", 500
+        return f"Error uploading files: {str(e)}", 500
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
